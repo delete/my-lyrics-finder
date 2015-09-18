784bb7459065ab5a8ceca5a45b8082dc6f266ad5
delete <pinheiro.llip@gmail.com>
2015-09-18 19:44:02 -0300

    unit test added

--- test/spec/services/vagalume.js
+++ test/spec/services/vagalume.js
@@ -2,17 +2,40 @@
 
 describe('Service: Vagalume', function () {
 
-  // load the service's module
-  beforeEach(module('letrasApp'));
+    // load the service's module
+    beforeEach(module('letrasApp'));
 
-  // instantiate service
-  var Vagalume;
-  beforeEach(inject(function (_Vagalume_) {
-    Vagalume = _Vagalume_;
-  }));
+    // instantiate service
+    var Vagalume;
+    beforeEach(inject(function (_Vagalume_) {
+        Vagalume = _Vagalume_;
+    }));
 
-  it('should do something', function () {
-    expect(!!Vagalume).toBe(true);
-  });
+    it('should return 5 lyrics data from vagalume api', function () {
+        var params = {
+            'scope': 'all',
+            'type': 'mus'
+        };
+
+        Vagalume.rank.get(params, function (data) {
+            expect(data).not.toBe(null);
+            expect(data.all.length).toBe(5);
+        });
+
+    });
+
+    it('should return Last Kiss lyrics from pearl jam', function () {
+        var params = {
+            'art': 'Pearl Jam',
+            'mus': 'Last Kiss'
+        };
+
+        Vagalume.search.get(params, function (data) {
+            expect(data).not.toBe(null);
+            expect(data.type).toBe('exact');
+            expect(data.art.name).toBe('Pearl Jam');
+            expect(data.mus[0].name).toBe('Last Kiss');
+        });
+    });
 
 });
