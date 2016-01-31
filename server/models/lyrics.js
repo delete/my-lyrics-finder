e97015bfc2102230cca5d6cb5a45d8ae8ec7ef51
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-01-30 23:05:41 -0200

    modeling Lyrics

--- server/models/lyrics.js
+++ server/models/lyrics.js
@@ -5,25 +5,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const lyricsSchema = new Schema({
-  art: String,
-  mus: String
-//  type: String,
-//  art: {
-//    id: String,
-//    name: String,
-//    url: String
-//  },
-//  mus: [{
-//    id: String,
-//    name: String,
-//    url: String,
-//    text: String,
-//    translate: [{
-//      id: String,
-//      url: String,
-//      text: String
-//    }]
-//  }]
+  type: String,
+  art: {
+    id: String,
+    name: String,
+    url: String
+  },
+  mus: [{
+    id: String,
+    name: String,
+    url: String,
+    text: String,
+    translate: [{
+      id: String,
+      url: String,
+      text: String
+    }]
+  }]
 });
 
 module.exports = mongoose.model('Lyrics', lyricsSchema);
