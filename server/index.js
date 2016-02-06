feb1b9235a7f394832f398b6236b02facf141394
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-02-06 16:26:18 -0200

    add Dockerfile and composer to run server and mongodb

--- server/index.js
+++ server/index.js
@@ -11,7 +11,15 @@ const URL = 'mongodb://192.168.1.8:27017/data'
 
 const PORT = 8080;
 
-mongoose.connect(URL);
+var MONGO_DB;
+const DOCKER_DB = process.env.DB_PORT;
+
+if ( DOCKER_DB ) {
+    MONGO_DB = DOCKER_DB.replace( 'tcp', 'mongodb' ) + '/data';
+} else {
+    MONGO_DB = process.env.MONGODB;
+}
+mongoose.connect(MONGO_DB || URL);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +29,6 @@ app.set('json spaces', 4);
 
 require('./routes')(app);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`List on port: ${PORT}`);
 });
