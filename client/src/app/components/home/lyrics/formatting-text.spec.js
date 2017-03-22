a0780b3ef990fe2901b1e0f658bdcbb05d70099b
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2017-03-22 20:33:52 -0300

    add more tests

--- client/src/app/components/home/lyrics/formatting-text.spec.js
+++ client/src/app/components/home/lyrics/formatting-text.spec.js
@@ -10,8 +10,17 @@ const formattingText = FormattingText(fakeSce);
 describe('Filter: FormattingText', () => {
 
   it('should change "\\n" for <br> tag from input text', () => {
+    let expected = 'Testing<br>this<br>for<br>sure!';
     let text = 'Testing\nthis\nfor\nsure!';
-    expect(formattingText(text)).toBe('Testing<br>this<br>for<br>sure!');
+    let actual = formattingText(text);
+    
+    expect(actual).toBe(expected);
+  });
+
+  it('should return undefined if nothing was given', () => {
+    let actual = formattingText();
+    
+    expect(actual).toBeUndefined();
   });
 
 });
